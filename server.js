7a58ec86633eb35c0cb7392ff00f95fa67091715
deksss <deksss@i.ua>
2017-05-19 16:28:59 +0300

    conect react boilerplate

--- server.js
+++ server.js
@@ -3,12 +3,16 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
-
+console.log(__dirname);
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
+;
+
+
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
+//  .use((req, res) => res.sendFile(INDEX) )
+  .use(express.static(path.join(__dirname, 'client')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
